4d6623aff305658f0223da3caaaf3a006135ccda
dutnals <dutnaks77@sookmyung.ac.kr>
2025-11-09 22:59:41 +0900

    03

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    int i = 0;
-    char str[4];
-    str[0] = 'a';
-    str[1] = 'b';
-    str[2] = 'c';
-    str[3] = '\0';
+    char src[] = "The Worst things to eat before";
+    char dst[100];
 
-    while (str[i] != '\0') {
-        printf("%c", str[i]);
-        i++;
-    }
+    strcpy(dst, src);
 
+    printf("copied string : %s\n", dst);
     system("PAUSE");
     return 0;
 }
-
+ 
